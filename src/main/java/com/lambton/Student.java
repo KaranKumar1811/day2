91bcf6adc40c553ef6e8455bdf6cb7f41552efd1
Karan Kumar <kumarkay.ca@gmail.com>
2019-09-19 19:10:39 -0400

    Create Student

---
+++ src/main/java/com/lambton/Student.java
@@ -0,0 +1,13 @@
+package com.lambton;
+
+import java.lang.reflect.Array;
+
+public class Student {
+    Integer studentID;
+    String firstName;
+    String lastName;
+    int[] MarksOf5 ;
+    int total;
+    double Percentage;
+    String Result;
+}
